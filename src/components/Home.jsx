b074fb274ab6523f724e3843ac2439dd6177c83d
CodingPoka <kingshukbd.01@gmail.com>
2025-12-09 16:30:00 +0600

    cange slide 6 image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import slide2 from "../assets/homeImage/slide2.jpg";
 import slide3 from "../assets/homeImage/slide3.png";
 import slide4 from "../assets/homeImage/slide4.jpg";
 import slide5 from "../assets/homeImage/slide5.png";
-import slide6 from "../assets/homeImage/slide6.jpeg";
+import slide6 from "../assets/homeImage/slide6.jpg";
 import slide7 from "../assets/homeImage/slide7.png";
 import slide8 from "../assets/homeImage/slide8.jpg";
 import section1 from "../assets/homeImage/section1.jpg";
